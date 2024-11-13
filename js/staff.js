df35975c0a7a1b41f9aadfd228a367b987370825
ariam2023 <movichabraham@gmail.coml.com>
2024-11-13 14:46:28 +0000

    json-lab

---
+++ js/staff.js
@@ -0,0 +1,16 @@
+/*var myObject = { name: "Martin" };
+console.dir(myObject);*/
+
+var myObject = { name : "Martin", email: "myemail@company.com"};
+console.dir(myObject);
+
+var bobDetails = {
+    name : 'Bob',
+    age : 21,
+    qualifications: {
+    gsce : true,
+    alevels : true,
+    degree : false
+    }
+ }
+ console.dir(bobDetails);
