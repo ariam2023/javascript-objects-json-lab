0d66159778ef05a37ab24b25ce0dd6de88519878
ariam2023 <movichabraham@gmail.coml.com>
2024-11-13 14:25:54 +0000

    json-lab

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,14 @@
 (function(){ 
+
+    //Create a new DIV
     console.info("Hello JS"); 
     const myNode = document.createElement("div");
     document.getElementById("digitalClock");
     digitalClock.appendChild(myNode);
     myNode.innerHTML = "Welcome";
+
+    //Date Object
+    const myDate = new Date();
+    console.dir(myDate);
+    myNode.innerHTML = myDate.toDateString();
     })();
