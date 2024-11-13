80bf933263def039fd9b029738cec56ae94087ce
ariam2023 <movichabraham@gmail.coml.com>
2024-11-13 14:49:34 +0000

    json-lab

--- js/staff.js
+++ js/staff.js
@@ -1,16 +1,50 @@
+//Objects
+
 /*var myObject = { name: "Martin" };
 console.dir(myObject);*/
 
-var myObject = { name : "Martin", email: "myemail@company.com"};
+var myObject = { name: "Martin", email: "myemail@company.com" };
 console.dir(myObject);
+
+var bobDetails = {
+    name: 'Bob',
+    age: 21,
+    qualifications: {
+        gsce: true,
+        alevels: true,
+        degree: false
+    }
+}
+console.dir(bobDetails);
+//objects inside Arrays
+
+var qualifications = [
+    {
+        subject: "Maths",
+        grade: "A"
+    },
+    {
+        subject: "English",
+        grade: "B"
+    },
+    {
+        subject: "French",
+        grade: "C"
+    },
+    {
+        subject: "Physics",
+        grade: "B"
+
+    }
+];
+
+//Methods inside Objects
 
 var bobDetails = {
     name : 'Bob',
     age : 21,
-    qualifications: {
-    gsce : true,
-    alevels : true,
-    degree : false
+    getInfo : function(){
+    return this.name + " " + this.age;
     }
  }
- console.dir(bobDetails);
+ console.info(bobDetails.getInfo);
